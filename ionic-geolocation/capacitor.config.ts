2491cc1843bd90c2a04b48fe58d9b872a3494a1d
unknown <pedro_ppc@hotmail.com>
2023-05-31 14:53:35 -0500

    commit

---
+++ ionic-geolocation/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-geolocation',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
